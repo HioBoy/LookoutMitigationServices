6bc0fb43dc185d5168cd96ff29880ee93bfa4b2c
Amit Mhatre <mhatre@amazon.com>
2014-05-13 14:12:36 -0700

    Keeping a track of POPs with Cisco router instead of keeping a track of POPs with Juniper router instead.

--- src/com/amazon/lookout/mitigation/service/workflow/helper/Route53SingleCustomerTemplateLocationsHelper.java
+++ src/com/amazon/lookout/mitigation/service/workflow/helper/Route53SingleCustomerTemplateLocationsHelper.java
@@ -16,27 +16,27 @@ import com.amazon.lookout.mitigation.service.MitigationModificationRequest;
 public class Route53SingleCustomerTemplateLocationsHelper implements TemplateBasedLocationsHelper {
     
     private final EdgeLocationsHelper locationsHelper;
-    private final Set<String> popsWithJuniperRouter;
+    private final Set<String> popsWithCiscoRouter;
     
-    @ConstructorProperties({"edgeLocationsHelper", "popsWithJuniperRouter"})
-    public Route53SingleCustomerTemplateLocationsHelper(@Nonnull EdgeLocationsHelper locationsHelper, @Nonnull Set<String> popsWithJuniperRouter) {
+    @ConstructorProperties({"edgeLocationsHelper", "popsWithCiscoRouter"})
+    public Route53SingleCustomerTemplateLocationsHelper(@Nonnull EdgeLocationsHelper locationsHelper, @Nonnull Set<String> popsWithCiscoRouter) {
         Validate.notNull(locationsHelper);
         this.locationsHelper = locationsHelper;
         
-        Validate.notNull(popsWithJuniperRouter); // Could be empty for beta.
-        this.popsWithJuniperRouter = popsWithJuniperRouter;
+        Validate.notNull(popsWithCiscoRouter); // Could be empty for beta.
+        this.popsWithCiscoRouter = popsWithCiscoRouter;
     }
     
     /**
-     * For templates that use this locations helper, locations for deployment are all non-blackwatch POPs which have Juniper routers.
+     * For templates that use this locations helper, locations for deployment are all non-blackwatch POPs which have a Juniper router (don't have Cisco router)
      * @param request MitigationModificationRequest instance sent by the client.
-     * @return Set<String> representing a set of POPs where BW doesn't have any hosts and the POP has Juniper routers.
+     * @return Set<String> representing a set of POPs where BW doesn't have any hosts and the POP has Juniper router.
      */
     @Override
     public Set<String> getLocationsForDeployment(MitigationModificationRequest request) {
         Set<String> locationsToDeploy = new HashSet<String>();
         for (String location : locationsHelper.getAllNonBlackwatchPOPs() ) {
-            if (popsWithJuniperRouter.contains(location.toUpperCase())) {
+            if (!popsWithCiscoRouter.contains(location.toUpperCase())) {
                 locationsToDeploy.add(location);
             }
         }
