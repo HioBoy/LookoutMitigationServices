b83258d8c3b5ca3e3a4b0f46fc59769c7d6fcaab
yinz <yinz@amazon.com>
2015-12-03 21:04:33 +0000

    do not throw nullpointerexception if get supported asns is null
    cr https://cr.amazon.com/r/4601849/

--- src/com/amazon/lookout/mitigation/service/activity/helper/BlackholeDeviceConverter.java
+++ src/com/amazon/lookout/mitigation/service/activity/helper/BlackholeDeviceConverter.java
@@ -3,33 +3,37 @@ package com.amazon.lookout.mitigation.service.activity.helper;
 import java.util.HashSet;
 import java.util.stream.Collectors;
 
+import org.springframework.util.CollectionUtils;
+
 import com.amazon.lookout.ddb.model.BlackholeDevice;
 import com.amazon.lookout.mitigation.service.BlackholeDeviceInfo;
 
 public class BlackholeDeviceConverter {
-    
+
     public static BlackholeDevice convertBlackholeDeviceInfoRequest(BlackholeDeviceInfo blackholeDeviceInfo) {
         BlackholeDevice blackholeDevice = new BlackholeDevice();
         blackholeDevice.setDeviceName(blackholeDeviceInfo.getDeviceName());
         blackholeDevice.setDeviceDescription(blackholeDeviceInfo.getDeviceDescription());
         blackholeDevice.setEnabled(blackholeDeviceInfo.isEnabled());
-        blackholeDevice.setSupportedASNs(new HashSet<Integer>(blackholeDeviceInfo.getSupportedASNs()));
+        if (!CollectionUtils.isEmpty(blackholeDeviceInfo.getSupportedASNs())) {
+            blackholeDevice.setSupportedASNs(new HashSet<Integer>(blackholeDeviceInfo.getSupportedASNs()));
+        }
         blackholeDevice.setVersion(blackholeDeviceInfo.getVersion());
         return blackholeDevice;
     }
-    
+
     public static BlackholeDeviceInfo convertBlackholeDeviceResponse(BlackholeDevice blackholeDevice) {
         BlackholeDeviceInfo blackholeDeviceInfo = new BlackholeDeviceInfo();
         blackholeDeviceInfo.setDeviceName(blackholeDevice.getDeviceName());
         blackholeDeviceInfo.setDeviceDescription(blackholeDevice.getDeviceDescription());
         blackholeDeviceInfo.setEnabled(blackholeDevice.isEnabled());
-        
-        // The sorting isn't needed but it does make the response more consistent
-        blackholeDeviceInfo.setSupportedASNs(
-                blackholeDevice.getSupportedASNs().stream().sorted().collect(Collectors.toList()));
-        
+        if (!CollectionUtils.isEmpty(blackholeDevice.getSupportedASNs())) {
+            // The sorting isn't needed but it does make the response more consistent
+            blackholeDeviceInfo.setSupportedASNs(
+                    blackholeDevice.getSupportedASNs().stream().sorted().collect(Collectors.toList()));
+        }
         blackholeDeviceInfo.setVersion(blackholeDevice.getVersion());
-        
+
         return blackholeDeviceInfo;
     }
 }
